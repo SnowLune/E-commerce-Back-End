26eea055236b6d79608ae08045a70fea87f58842
Luna R <snowluna@protonmail.com>
2022-07-26 16:51:18 -0400

    fix bug in tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -33,7 +33,7 @@ router.get('/:id', (req, res) => {
       }
    )
       .then( ( tag ) => res.status( 200 ).json( tag ) )
-      .catch( ( err ) => res.status.( 404 ).json( err ) );
+      .catch( ( err ) => res.status( 404 ).json( err ) );
 });
 
 router.post('/', (req, res) => {
