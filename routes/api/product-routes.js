540d83f177e62c9a69369dc51cbb1210883b3b16
Luna R <snowluna@protonmail.com>
2022-07-26 20:14:46 +0000

    finish setting up routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,33 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+   Product.findAll(
+      {
+         include: [ Category, { model: Tag, through: ProductTag } ]
+      }
+   )
+      .then( products => res.json( products ) )
+      .catch( err => {
+         console.error( err );
+         res.status( 500 ).json( err )
+      } );
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+   Product.findOne(
+      {
+         where: { id: req.params.id },
+         include: [ Category, { model: Tag, through: ProductTag } ]
+      }
+   )
+      .then( products => res.json( products ) )
+      .catch( err => {
+         console.error(err);
+         res.status( 400 ).json( err );
+      } )
 });
 
 // create new product
@@ -28,7 +49,7 @@ router.post('/', (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagIds && req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -91,6 +112,18 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+   Product.destroy(
+      {
+         where: { id: req.params.id }
+      }
+   )
+      .then( products => {
+         res.json( products );
+      } )
+      .catch( err => {
+         console.error( err );
+         res.status( 400 ).json( err );
+      } );
 });
 
 module.exports = router;
